21892e63c75f19ba32f81a69a654b5c431963cd3
Flipper <philip.schaefer@gmail.com>
2025-04-29 00:29:24 +0200

    fix - db string doesnt work

--- src/db/drizzle.config.ts
+++ src/db/drizzle.config.ts
@@ -5,7 +5,9 @@ export default {
   out: './src/db/migration',
   dialect: 'postgresql',
   dbCredentials: {
-    url: `postgresql://${process.env.DBhost}:${parseInt(<string>process.env.DBport, 10) || 3306}/${process.env.DBusername}`,
+    host: process.env.DBhost,
+    port: parseInt(<string>process.env.DBport, 10) || 5432,
+    database: process.env.DBusername,
     user: process.env.DBusername,
     password: process.env.DBpassword,
   },
