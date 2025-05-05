b114416ef6a558f3651e27bda38012e0ee5d8a4d
Phil <philip.schaefer0@gmail.com>
2025-05-06 01:18:34 +0200

    moved - changed file naming scheme

--- src/listeners/readyLogin.ts
+++ src/listeners/readyLogin.ts
@@ -1,12 +1,13 @@
 import { Listener } from '@sapphire/framework';
 
-import type { Client } from 'discord.js';
+import { type Client, Events } from 'discord.js';
 
-export class ReadyListener extends Listener {
+export class ReadyListener extends Listener<typeof Events.ClientReady> {
   public constructor(context: Listener.LoaderContext, options: Listener.Options) {
     super(context, {
       ...options,
       once: true,
+      event: Events.ClientReady,
     });
   }
 
